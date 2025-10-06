07a3a9feeb8c0cfc9e4778ee0f88e6c4e660306e
joharry <joharrywork@gmail.com>
2025-10-06 10:30:14 +0900

    editing the js

--- Kick_Boxing/Javascript/home.js
+++ Kick_Boxing/Javascript/home.js
@@ -1,12 +1,8 @@
 // hamburger menu
 
 // Selecting DOM items
-
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".bgnav-item");
 
 // Set the initial state of the menu
 let showMenu = false;
@@ -15,24 +11,13 @@ let showMenu = false;
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if (!showMenu) {
+    showMenu = !showMenu; // Toggle the state
+
+    if (showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add("show");
-        menuNav.classList.add("show");
-        menuBranding.classList.add("show");
-        navItems.forEach((item) => item.classList.add("show"));
-
-        // Set menu state
-        showMenu = true;
     } else {
         menuBtn.classList.remove("close");
         menu.classList.remove("show");
-        menuNav.classList.remove("show");
-        menuBranding.classList.remove("show");
-        navItems.forEach((item) => item.classList.remove("show"));
-
-        //set menu state to clased
-        showMenu = false;   
     }
-
 }
